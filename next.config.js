641d8674b6221bce17916cba3d4ee66f035caf3c
Thommy Kane <tkane@foodandtravel.net>
2026-02-26 10:38:39 -0800

    Remove zod; add serverExternalPackages for pdfjs-dist

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ protocol: "https", hostname: "*" }, { protocol: "http", hostname: "*" }] },
+  // Don't bundle pdfjs-dist in serverless functions (used only in client MagazineFlipbook)
+  serverExternalPackages: ["pdfjs-dist"],
 };
 
 module.exports = nextConfig;
